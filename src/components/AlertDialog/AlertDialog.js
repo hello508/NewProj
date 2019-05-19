aaa2d679e05aa8b3d7b125211d30a74da4a03f4f
Anil Katta <anilkatta@Anils-MacBook-Pro.local>
2019-05-19 15:44:28 -0500

    changed to functional components

--- src/components/AlertDialog/AlertDialog.js
+++ src/components/AlertDialog/AlertDialog.js
@@ -6,35 +6,31 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 
-class AlertDialog extends React.Component {
-  state = {}
-
-  render() {
-    return (
-      <div>
-        <Dialog
-          open={this.props.open}
-          onClose={this.props.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{'WARNING!'}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              Let Google help apps determine location. This means sending anonymous location data to Google, even when
-              no apps are running.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary">Disagree</Button>
-            <Button color="primary" autoFocus>
-              Agree
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </div>
-    )
-  }
+const AlertDialog = (props) => {
+  return (
+    <div>
+      <Dialog
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'WARNING!'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to Google, even when no
+            apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary">Disagree</Button>
+          <Button color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
 }
 
 export default AlertDialog
