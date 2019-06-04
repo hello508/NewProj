6a1d238175caca0a61628421038581e713401b3c
Anil Katta <anilkatta@Anils-MacBook-Pro.local>
2019-06-03 21:46:29 -0500

    changes to approve/reject

--- src/common/http.js
+++ src/common/http.js
@@ -1,15 +1,15 @@
-export async function createPostRequest(requestBody = {}) {
+export async function createPostRequest(requestBody = {}, url) {
   const nbk = localStorage.getItem('nbk');
   const data = JSON.stringify({
     userName: nbk,
-    ...requestBody
+    ...requestBody,
   })
   const env ='dev'
   if (nbk !== undefined && nbk !== null) {
     const fetchURL = await fetch(`http://boa.co:3000/${env}.ficc.ui.middletier/`, {
       credentials: 'include',
     )}
-    const response = await fetch(`${fetchURL.url}${env}.ficc.fictools.ui/getCards`, {
+    const response = await fetch(`${fetchURL.url}${env}.ficc.fictools.ui/${url}`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
